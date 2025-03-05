bdf9cc489cc1e5b4db881020187c5458b83ca953
jasondrogba <757104667@qq.com>
2025-03-05 16:07:33 +0800

    remove log

--- monitor-remove-offline-worker.py
+++ monitor-remove-offline-worker.py
@@ -15,7 +15,7 @@ ETCD_POD = "alluxio-etcd-0"
 # 会进入到coordinator pod 中执行alluxio info nodes命令，可以换成其他正常running的worker pod
 COORDINATOR_POD = "alluxio-coordinator-0"
 # 日志信息保存的地方
-LOG_FILE = "worker_cleanup.log"
+LOG_FILE = "worker-monitor.log"
 # 定时执行的时间，每隔多长时间监测node，worker状态
 CHECK_INTERVAL = 30  
 
